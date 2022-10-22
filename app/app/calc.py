01771d89ff5e1b14e0de79036f0d41eac01f8e8e
songpon lekpetch <songpon@peerpower.co.th>
2022-10-22 22:12:24 +0700

    Add db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(a, b):
+    """ADD a and b return result"""
+    return a + b
+
+def subtract(a, b):
+    """Subtract a and b and return result."""
+    return b - a
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample test
+"""
+from django.test import TestCase
+
+from app import calc
+
+
+class CalcTests(TestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtract numbers."""
+        res = calc.subtract(5,6)
+
+        self.assertEqual(res, 1)
